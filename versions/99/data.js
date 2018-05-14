5a606e3c35499b3e18f0236ab810daf83d505c0a
Emeric Francis <Emeric.Francis@landregistry.gov.uk>
2018-05-14 10:38:57 +0100

    adding intial code to repo

---
+++ versions/99/data.js
@@ -0,0 +1,191 @@
+var data = {
+	"version": "Current service (beta). January 2018",
+	"last-updated": "08 January 2018",
+	"change-history": [
+		"list changes in version here",
+		"e.g. Title wording changed"
+	],
+	"userjourneys": [{
+			"title": "User Journey (Scenario) 1",
+			"path": [{
+					"caption": "Start",
+					"imgref": "images/get-property-information/01-start-on-govuk.png",
+					"note": [
+						"New service for basic searches",
+						"Not Green button (that's Find A Property - older service)"
+					]
+				},
+				{
+					"caption": "Search",
+					"imgref": "images/get-property-information/02-search.png",
+					"note": "Postcode only."
+				},
+				{
+					"caption": "Select address",
+					"imgref": "images/get-property-information/03-results.png",
+					"note": [
+						"All the addresses in a postcode.",
+						"Up to 100 addresses",
+						"Places with postal addresses, not 'land at'",
+						"more examples of results and property page below"
+					]
+				},
+				{
+					"caption": "Select property record (title)",
+					"imgref": "images/get-property-information/04-property-page.png",
+					"note": [
+						"The property titles for an address *",
+						"* all that we can currently link to this address (percent?)",
+						"first version of this page",
+						"more examples of results and property page below"
+					]
+
+				},
+				{
+					"caption": "Property title",
+					"imgref": "images/get-property-information/05-about-property-title.png",
+					"note": "What you'll get if you continue"
+
+				},
+				{
+					"caption": "Tell us who you are",
+					"imgref": "images/get-property-information/06b-sign-in.png",
+					"note": [
+						"Create an account or sign-in",
+						"Create account more likely - 70% to 30%",
+						"more account function shown below"
+					]
+				},
+				{
+					"caption": "Confirm property title wanted",
+					"imgref": "images/get-property-information/07-confirm-purchase.png",
+					"note": [
+						"What you're getting when you pay"
+					]
+				},
+				{
+					"caption": "Payment - part 1",
+					"imgref": "images/get-property-information/08-worldpay-1.png",
+					"note": "Select card"
+				},
+				{
+					"caption": "Payment - part 2",
+					"imgref": "images/get-property-information/08-worldpay-2.png",
+					"note": "card details"
+				},
+				{
+					"caption": "Payment - part 3",
+					"imgref": "images/get-property-information/08-worldpay-3.png",
+					"note": "Confirmation"
+				},
+				{
+					"caption": "Summary details",
+					"imgref": "images/get-property-information/09-summary.png",
+					"note": [
+						"PDF version to 'take away'",
+						"can view this for an hour, but cannot reaccess this again without paying"
+					]
+				},
+				{
+					"caption": "What next?",
+					"imgref": "images/get-property-information/10-signout.png",
+					"note": [
+						"Sign out is one thing you can do after viewing the summary"
+					]
+				}
+
+			]
+		},
+		{
+			"title": "User Journey (Scenario) 2",
+			"path": [{
+					"caption": "Start",
+					"imgref": "images/get-property-information/01-start-on-govuk.png",
+					"note": [
+						"New service for basic searches",
+						"Not Green button (that's Find A Property - older service)"
+					]
+				},
+				{
+					"caption": "Search",
+					"imgref": "images/get-property-information/02-search.png",
+					"note": "Postcode only."
+				},
+				{
+					"caption": "Select address",
+					"imgref": "images/get-property-information/03-results.png",
+					"note": [
+						"All the addresses in a postcode.",
+						"Up to 100 addresses",
+						"Places with postal addresses, not 'land at'",
+						"more examples of results and property page below"
+					]
+				},
+				{
+					"caption": "Select property record (title)",
+					"imgref": "images/get-property-information/04-property-page.png",
+					"note": [
+						"The property titles for an address *",
+						"* all that we can currently link to this address (percent?)",
+						"first version of this page",
+						"more examples of results and property page below"
+					]
+
+				},
+				{
+					"caption": "Property title",
+					"imgref": "images/get-property-information/05-about-property-title.png",
+					"note": "What you'll get if you continue"
+
+				},
+				{
+					"caption": "Tell us who you are",
+					"imgref": "images/get-property-information/06b-sign-in.png",
+					"note": [
+						"Create an account or sign-in",
+						"Create account more likely - 70% to 30%",
+						"more account function shown below"
+					]
+				},
+				{
+					"caption": "Confirm property title wanted",
+					"imgref": "images/get-property-information/07-confirm-purchase.png",
+					"note": [
+						"What you're getting when you pay"
+					]
+				},
+				{
+					"caption": "Payment - part 1",
+					"imgref": "images/get-property-information/08-worldpay-1.png",
+					"note": "Select card"
+				},
+				{
+					"caption": "Payment - part 2",
+					"imgref": "images/get-property-information/08-worldpay-2.png",
+					"note": "card details"
+				},
+				{
+					"caption": "Payment - part 3",
+					"imgref": "images/get-property-information/08-worldpay-3.png",
+					"note": "Confirmation"
+				},
+				{
+					"caption": "Summary details",
+					"imgref": "images/get-property-information/09-summary.png",
+					"note": [
+						"PDF version to 'take away'",
+						"can view this for an hour, but cannot reaccess this again without paying"
+					]
+				},
+				{
+					"caption": "What next?",
+					"imgref": "images/get-property-information/10-signout.png",
+					"note": [
+						"Sign out is one thing you can do after viewing the summary"
+					]
+				}
+
+			]
+		}
+	]
+}
