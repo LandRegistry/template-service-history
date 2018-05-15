636ec9423856ee4a44e835cf05c3ab7363fa0094
Emeric Francis <Emeric.Francis@landregistry.gov.uk>
2018-05-15 07:38:38 +0100

    Getting rid of extra images that are not needed

--- versions/01/data.js
+++ versions/01/data.js
@@ -5,24 +5,24 @@ var data = {
 			"title": "User Journey (Scenario) 1",
 			"path": [{
 					"caption": "Start Page",
-					"imgref": "images/bike-reported/01-start.png",
+					"imgref": "images/01-start.png",
 					"note": []
 				},
 				{
 					"caption": "Reported",
-					"imgref": "images/bike-reported/02-reported.png",
+					"imgref": "images/02-reported-yes.png",
 					"note": "If bike is already reported in future iterations they will be able to start a insurance claim."
 				},
 				{
 					"caption": "Crime Reference",
-					"imgref": "images/bike-reported/03-crime-reference.png",
+					"imgref": "images/03-crime-reference.png",
 					"note": [
 						"Crime reference number used to gather detials already provided to the police"
 					]
 				},
 				{
 					"caption": "Personal Details",
-					"imgref": "images/bike-reported/04-personal-detials.png",
+					"imgref": "images/04-personal-detials.png",
 					"note": [
 						"User checks detials attached to the crime reference number are correct",
 						"Compulosry legal message at the bottom"
@@ -30,7 +30,7 @@ var data = {
 				},
 				{
 					"caption": "Confirmation",
-					"imgref": "images/bike-reported/05-confirmation.png",
+					"imgref": "images/05-confirmation.png",
 					"note": [
 						"Users felt more reassured if they were going to recieve a email"
 					]
@@ -41,26 +41,26 @@ var data = {
 			"title": "User Journey (Scenario) 2",
 			"path": [{
 					"caption": "Start Page",
-					"imgref": "images/bike-reported-at-location/01-start.png",
+					"imgref": "images/01-start.png",
 					"note": []
 				},
 				{
 					"caption": "Reported",
-					"imgref": "images/bike-reported-at-location/02-reported.png",
+					"imgref": "images/02-reported-no.png",
 					"note": [
 						"the user can report the crime online and get a crime reference number"
 					]
 				},
 				{
 					"caption": "Location",
-					"imgref": "images/bike-reported-at-location/03-location.png",
+					"imgref": "images/03-location.png",
 					"note": [
 						"**user will recieve pop-up in top left asking for access to thier location**"
 					]
 				},
 				{
 					"caption": "Geo-Location",
-					"imgref": "images/bike-reported-at-location/04-geo-location.png"
+					"imgref": "images/04-geo-location.png"
 				}
 			]
 		}
